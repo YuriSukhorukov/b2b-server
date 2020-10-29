9cdcbd7096cabaaa15b9c0b6db9b7006f2b4909c
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-10-29 10:05:07 +0300

    edit: add AddUser struct, validation

--- model/user.go
+++ model/user.go
@@ -1,5 +1,28 @@
 package model
 
+import "errors"
+
+type AddUser struct {
+	Email   	string  `json:"email" example:"my_email@gmail.com" format:"string" header:"email"`
+	Password 	string 	`json:"password" example:"pa!ssvv0rD" format:"string" header:"password"`
+}
+
+var (
+	ErrEmailInvalid = errors.New("email is empty")
+	ErrPasswordInvalid = errors.New("password is empty")
+)
+
+func (a AddUser) Validation() error {
+	switch {
+		case len(a.Email) == 0:
+			return ErrEmailInvalid
+		case len(a.Password) == 0:
+			return ErrPasswordInvalid
+		default:
+			return nil
+	}
+}
+
 type User struct {
     UserID       string `db:"user_id"`
     Email		 string `db:"email"`
